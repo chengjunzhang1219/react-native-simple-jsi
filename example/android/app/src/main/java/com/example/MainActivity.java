84b2ca2f75b95b1c52e8b30509e30d6bcc6e7c54
chengjunzhang <89475067+chengjunzhang61@users.noreply.github.com>
2022-02-15 00:34:06 -0800

    init commit

--- example/android/app/src/main/java/com/example/MainActivity.java
+++ example/android/app/src/main/java/com/example/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.rnjsisecond;
+package com.example;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RnJsiSecondExample";
+    return "example";
   }
 }
--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  RnJsiSecondExample
-//
-
-import Foundation
